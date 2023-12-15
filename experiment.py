cded3b59cbef4b15775ab2a0f81dda3cd0ef7e30
ayhem18 <bouabidayhem@gmail.com>
2023-12-15 16:32:13 +0300

    experiment 1 fixed

--- experiment.py
+++ experiment.py
@@ -25,6 +25,8 @@ class L1Norm(ProximityOperator):
 class Linear(Model):
     def __init__(self, in_features: int, out_features: int) -> None:
         super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
         self._W = np.random.randn(in_features, out_features)
         # self._b = np.random.randn(out_features)
 
@@ -33,10 +35,32 @@ class Linear(Model):
         return self._W
 
     def forward(self, x: Vector) -> Vector:
-        return x @ self._W
+        # the assumption here is that 
+        if x.ndim > 2: 
+            raise ValueError(f"Please make sure the input is 2 dimensionsal. Found: {x.ndim} dimensions")        
+
+        batch_size, features = x.shape
+        if features != self.in_features: 
+            raise ValueError(f"The input is expected to have: {self.in_features} features")
+
+        output = x @ self._W
+
+        assert output.shape == (batch_size, self.out_features), f"Make sure the output is of the correct shape. Found: {batch_size, self.out_features}. Found: {x.shape}"
+        return output
+
     
     def backward(self, x: Vector, upstream: Vector) -> Vector:
-        return upstream @ self._W.T
+        output = self.forward(x)
+        # make sure the upstream grad is of the exact same shape
+        assert upstream.shape == output.shape, f"Make sure the upstream gradient has the same as the output of the forward pass. Found: {upstream.shape}, Expected: {output.shape}"
+        
+        # calculate 'grad'
+        grad = x.T @ upstream
+        # make sure the grad is of the exact same shape as the weights  
+        assert grad.shape == self._W.shape, f"Make sure the shape of the grad is the same as the weights. Found: {grad.shape}. Expected: {self._W.shape}"
+
+        return grad
+
 
     def update(self, params: Vector) -> None:
         # self._W = params[:self._W.size].reshape(self._W.shape)
@@ -46,7 +70,9 @@ class Linear(Model):
 class SimpleDataset(DataLoader):
     def __init__(self, in_features: int, out_features: int) -> None:
         super().__init__()
+        # Each row represents a sample
         self._X = np.random.randn(100, in_features)
+        # the output will be of the shape: (100, out_features) (why out_features in general and not only '1' ?)
         self._y = self._X @ np.random.randn(in_features, out_features)
 
     def total_size(self) -> int:
@@ -63,9 +89,16 @@ class MSE(LossFunction):
 
     def dloss(self, j: int, size: int) -> Vector:
         """Gradient of MSE loss function w.r.t. model parameters"""
+        # extract the data and the labels
         X, y = self._dl.get(j, size)
+        # forward pass: predict
         y_hat = self._M.forward(X)
-        return self._M.backward(X, y_hat - y).mean(axis=0)
+
+        # MSE LOSS will be defined as the average of the squared error between y_hat and y
+        # so the gradient with respect to each element in 'y_hat' is the difference between y_hat_i and y_i divided by the number of elements in 'y' 
+        mse_loss_grad = (y_hat - y) / y.size
+        # why mean ???
+        return self._M.backward(X, mse_loss_grad)# .mean(axis=0) 
         
     def loss(self, j: int, size: int) -> float:
         """MSE loss function"""
@@ -75,18 +108,19 @@ class MSE(LossFunction):
     
     
 optimizer = ProxSkip(
-    models=Linear(10, 10),
-    dataloaders=SimpleDataset(10, 10),
+    models=Linear(10, 1),
+    dataloaders=SimpleDataset(10, 1),
     loss=MSE,
     prox=L1Norm(),
     num_iterations=1000,
-    learning_rate=0.001,
+    learning_rate=0.002,
     batch_size=100,
-    p=0.5
+    p=1
 )
 
 while (x_t := optimizer.step()):
     optimizer.update()
     print([x for x in optimizer._step['losses'][-1]][0])
+    print("#" * 100)
     
 print(optimizer._step['x_t'])
--- proxskip/optimizer.py
+++ proxskip/optimizer.py
@@ -85,6 +85,7 @@ class ProxSkip(Optimizer):
         self.p_ = p
 
     def step(self) -> Vector:
+        # extract the 
         t = self._step['t']
 
         if t >= self._num_iterations:
@@ -97,11 +98,12 @@ class ProxSkip(Optimizer):
         batch_idx = (t % self._total_size) // self._batch_size
         n = batch_idx * self._batch_size
 
-        # Flip a coin to decide whether to skip the prox step
-        to_skip = np.random.rand() > self.p_
+        # Flip a coin to decide whether to carry out with the prox opreation or not
+        to_prox = np.random.rand() > self.p_
         
-        # Calculate losses
-        l_ = [
+        # calculate the gradients of the loss function
+        # with respect to the parameters of each model
+        current_gradients = [
             self.loss_[i].dloss(n, self._batch_size)
             for i in range(len(self.models_))
         ]
@@ -110,17 +112,34 @@ class ProxSkip(Optimizer):
         phi_ = self._learning_rate / self.p_
         iphi_ = 1 / phi_
         
+        # calculate x^{t + 1} for every model / device: Line 3 in the algorithm
         x_h_tp1 = [
-            x_t[j] - self._learning_rate * (l_[j] - h_t[j])
+            x_t[j] - self._learning_rate * (current_gradients[j] - h_t[j])
             for j in range(len(self.models_))
         ]
-        if to_skip:
-            x_tp1 = x_h_tp1
+
+        if to_prox:
+            # in the federated learning setting, 
+            # the prox operation is averaging the values at the local devices
+
+            # step 1: average all x_h_tp1: since we do not know the exact shape
+            avg_weight = np.zeros(shape=x_h_tp1[0].shape)
+
+            for local_weight in x_h_tp1: 
+                avg_weight += local_weight
+
+            avg_weight /= len(x_h_tp1)
+
+            # set all the values in x_h_tp1 to 'avg_weight'
+            x_tp1 = [avg_weight for _ in x_h_tp1]
+
+            # x_tp1 = [
+            #     self.prox_(x_h_tp1[j] - phi_ * h_t[j], self._step)
+            #     for j in range(len(self.models_))
+            # ]
+
         else:
-            x_tp1 = [
-                self.prox_(x_h_tp1[j] - phi_ * h_t[j], self._step)
-                for j in range(len(self.models_))
-            ]
+            x_tp1 = x_h_tp1
 
         # for i, model in enumerate(self.models_):
         #     model.update(x_tp1[i])
@@ -144,3 +163,4 @@ class ProxSkip(Optimizer):
     def update(self) -> None:
         for j, model in enumerate(self.models_):
             model.update(self._step['x_t'][j])
+ 
